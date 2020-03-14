3d27e48a03353e5927e4cc9d43306d9713c04b09
MengZe <59787257+sudo-MEnGzE@users.noreply.github.com>
2020-03-14 19:55:44 +0800

    Update home.js

--- shared/scripts/home.js
+++ shared/scripts/home.js
@@ -9,6 +9,8 @@ document.querySelector(".home-blurb-action").addEventListener("click", () => {
   document.querySelector(".global-hamburger").classList.add("global-hamburger-state-visible");
   document.querySelector(".github-fork-ribbon").classList.remove("github-fork-ribbon-state-visible");
   document.querySelector(".github-fork-ribbon").classList.add("github-fork-ribbon-state-hidden");
+  document.querySelector(".global-clicking-space-1").classList.remove("global-clicking-space-1-state-hidden");
+  document.querySelector(".global-clicking-space-1").classList.add("global-clicking-space-1-state-visible");
 });
 
 document.querySelector(".global-dropdown-content").addEventListener("click", () => {
